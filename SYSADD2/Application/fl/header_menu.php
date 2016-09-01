5a7d67521a51b4682070e16ece69a5a98b0d48a4
skyraphaell <strapMasky@gmail.com>
2016-09-01 09:04:17 +0800

    Header

--- SYSADD2/Application/fl/header_menu.php
+++ SYSADD2/Application/fl/header_menu.php
@@ -5,10 +5,12 @@
     <table width="100%">
     <tr>
         <td class="menu" width="100"> <a <?php echo $target;?> class="menu" href="/<?php echo BASE_DIRECTORY;?>/main.php">  HOME  </a> </td>
+		<td class="menu" width="100"> <a <?php echo $target;?> class="menu" href="/<?php echo BASE_DIRECTORY;?>/module/page1.php">  MODULE  </a> </td>
         <td class="menu" width="100"> <a <?php echo $target;?> class="menu" href="/<?php echo BASE_DIRECTORY;?>/change_password.php">  PASSWORD  </a> </td>
         <td class="menu" width="100"> <a <?php echo $target;?> class="menu" href="/<?php echo BASE_DIRECTORY;?>/change_skin.php">  SKIN  </a> </td>
         <td class="menu" width="100"> <a <?php echo $target;?> class="menu" href="/<?php echo BASE_DIRECTORY;?>/about.php">  ABOUT  </a> </td>
         <td class="menu" width="100"> <a <?php echo $target;?> class="menu" href="/<?php echo BASE_DIRECTORY;?>/help/contents.php">  HELP  </a> </td>
+		
         <td align="right"><span class="text-normal">You are logged in as</span> <span class="text-info"><?php echo htmlentities($_SESSION['user'], ENT_COMPAT, MULTI_BYTE_ENCODING);?></span>&nbsp;</td>
         <td class="menu" width="75"> <a target="_parent" onClick="return confirm('Are you sure you wish to logout?')" href='/<?php echo BASE_DIRECTORY;?>/end.php' class="menu">  [LOGOUT]  </a> </td>
     </tr>
